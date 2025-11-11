a15a939ab76f727b867b7ebcc80dcf09ed8f9708
zevaca <logunik@bk.ru>
2025-11-11 21:11:59 +0700

    first databe input and output

---
+++ auth_service/go.mod
@@ -0,0 +1,14 @@
+module articles
+
+go 1.24.2
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.19.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.6 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+)
---
+++ auth_service/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"os"
+)
+
+func main() {
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var email, username, passwordHash string
+
+	err = conn.QueryRow(context.Background(),
+		"SELECT email, username, password_hash FROM users WHERE id=$1", 1).
+		Scan(&email, &username, &passwordHash)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Email: %s, Username: %s, Password Hash: %s\n", email, username, passwordHash)
+}
